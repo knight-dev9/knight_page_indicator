b76ee8f94ed0ae48d3225cf141641b2bc0cb04ee
Atirek Pothiwala <atirekpothiwala@gmail.com>
2025-02-28 22:28:53 +0530

    Indicator Style updated

--- lib/indicator_style.dart
+++ lib/indicator_style.dart
@@ -8,5 +8,30 @@ enum IndicatorStyle {
   line,
   horizontalPipe,
   verticalPipe,
-  widget,
+  widget;
+
+  String name() {
+    switch (this) {
+      case IndicatorStyle.circle:
+        return "Circle";
+      case IndicatorStyle.circleDuet:
+        return "Circle Duet";
+      case IndicatorStyle.circleDrop:
+        return "Circle Drop";
+      case IndicatorStyle.square:
+        return "Square";
+      case IndicatorStyle.squareDuet:
+        return "Square Duet";
+      case IndicatorStyle.rectangle:
+        return "Rectangle";
+      case IndicatorStyle.line:
+        return "Line";
+      case IndicatorStyle.horizontalPipe:
+        return "Horizontal Pipe";
+      case IndicatorStyle.verticalPipe:
+        return "Vertical Pipe";
+      case IndicatorStyle.widget:
+        return "Widget";
+    }
+  }
 }
